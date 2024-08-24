15bc256b45e5defa50546c2f10f8469f425ce03f
m4stika <m4stika@gmail.com>
2024-08-25 00:52:37 +0800

    main: remove promo assets

--- assets/images/promo/index.ts
+++
@@ -1,8 +0,0 @@
-import promo01 from "./promo01.jpg"
-import promo02 from "./promo02.jpg"
-import promo03 from "./promo03.jpg"
-import promo04 from "./promo04.jpg"
-import promo05 from "./promo05.jpg"
-import promo06 from "./promo06.jpg"
-
-export { promo01, promo02, promo03, promo04, promo05, promo06 }
